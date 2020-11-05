e821cc9712318911768a6aff34ea4458785d2273
JoseContreras <jocontrera16@gmail.com>
2020-11-04 23:10:59 -0300

    approotdir created

--- your_first_express_application/app.mjs
+++ your_first_express_application/app.mjs
@@ -1,41 +1,16 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+ import {default as express} from 'express';
+ import {default as hbs} from 'hbs';
+ import * as path from 'path';
+ // import * as favicon from 'server-favico';
+ import {default as logger} from 'morgan';
+ import {default as cookieParser} from 'cookie-parser';
+ import {default as bodyParser} from 'body-parser';
+ import * as http from 'http';
+ import {approotdir} from './approotdir.mjs';
+ const __dirname =  approotdir;
+import {
+  normalizePort, onError, onListening, handle404, basicErrorHandler
+} from './appsupport.mjs';
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+import { router as indexRouter } from './routes/index.mjs';
+// import {router as notesRouter} from './routes/notes.mjs';
