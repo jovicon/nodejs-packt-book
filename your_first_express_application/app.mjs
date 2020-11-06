9af19661b91830e71d0ce9a5ef039d2280de447b
JoseContreras <jocontrera16@gmail.com>
2020-11-05 22:56:47 -0300

    adding notes class

--- your_first_express_application/app.mjs
+++ your_first_express_application/app.mjs
@@ -14,3 +14,36 @@ import {
 
 import { router as indexRouter } from './routes/index.mjs';
 // import {router as notesRouter} from './routes/notes.mjs';
+
+export const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+hbs.registerPartials(path.join(__dirname, 'partials'));
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Router function lists
+app.use('/', indexRouter);
+app.use('/notes', notesRouter);
+
+// error handlers
+// catch 404 and forward to error handler
+app.use(handle404);
+app.use(basicErrorHandler);
+
+export const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+export const server = http.createServer(app);
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
